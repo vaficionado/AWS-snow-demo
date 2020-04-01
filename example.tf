92fdbeb44eca2a7ffa93d686d113a5ad4a2cb7a1
Jon Schulman <25127543+vaficionado@users.noreply.github.com>
2020-04-01 14:40:45 -0700

    new ami identifier mechanism

--- example.tf
+++ example.tf
@@ -1,7 +1,25 @@
 provider "aws" {
+    version = "~> 2.0"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-00bf61217e296b409"
-  instance_type = "t2.micro"
-  }
+data "aws_ami" "ubuntu" {
+    most_recent = true
+
+    filter {
+        name = "name"
+        values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+    }
+
+    filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+    }
+
+    owners = ["099720109477"]
+}
+
+    resource "aws_instance" "web" {
+        ami                     =       "${data.aws_ami.ubuntu.id}"
+        instance_type           =       "t2.micro"
+        key_name                =       "${var.key_pair_name}"
+    }
